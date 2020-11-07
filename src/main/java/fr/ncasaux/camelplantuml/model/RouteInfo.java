1081cd8a7ca061710327bf9d74c34691bd6b3a74
ncasaux <nicolas.apo@gmail.com>
2020-11-07 15:07:10 +0100

    Added some final

--- src/main/java/fr/ncasaux/camelplantuml/model/RouteInfo.java
+++ src/main/java/fr/ncasaux/camelplantuml/model/RouteInfo.java
@@ -1,8 +1,8 @@
 package fr.ncasaux.camelplantuml.model;
 
 public class RouteInfo {
-    private String description;
-    private String diagramElementId;
+    private final String description;
+    private final String diagramElementId;
 
     public RouteInfo(String description, String diagramElementId) {
         this.description = description;
