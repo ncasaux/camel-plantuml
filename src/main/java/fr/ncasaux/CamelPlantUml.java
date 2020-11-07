1ca92f6a255bdfe20a554005f12fca01dd983b43
ncasaux <nicolas.apo@gmail.com>
2020-11-07 13:55:18 +0100

    Polish

--- src/main/java/fr/ncasaux/CamelPlantUml.java
+++ src/main/java/fr/ncasaux/CamelPlantUml.java
@@ -7,7 +7,6 @@ public class CamelPlantUml {
     public static void main(String... args) throws Exception {
         Main main = new Main();
         main.configure().addRoutesBuilder(new CamelPlantUmlRouteBuilder("localhost",9090));
-        main.configure().addRoutesBuilder(new TestRouteBuilder());
         main.run(args);
     }
 }
