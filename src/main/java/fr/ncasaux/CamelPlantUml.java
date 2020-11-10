604cc59b1a8761e3a5dd12c15517d743a298c9c4
ncasaux <nicolas.apo@gmail.com>
2020-11-10 23:52:53 +0100

    fix main

--- src/main/java/fr/ncasaux/CamelPlantUml.java
+++ src/main/java/fr/ncasaux/CamelPlantUml.java
@@ -6,7 +6,7 @@ public class CamelPlantUml {
 
     public static void main(String... args) throws Exception {
         Main main = new Main();
-        main.configure().addRoutesBuilder(new CamelPlantUmlRouteBuilder("localhost",9090));
+        main.addRouteBuilder(new CamelPlantUmlRouteBuilder());
         main.run(args);
     }
 }
