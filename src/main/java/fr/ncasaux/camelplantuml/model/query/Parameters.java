e7b42604969eabfe30111e1427bd183d41df4960
ncasaux <nicolas.apo@gmail.com>
2020-11-13 18:29:05 +0100

    Polish Added some more logs

---
+++ src/main/java/fr/ncasaux/camelplantuml/model/query/Parameters.java
@@ -0,0 +1,14 @@
+package fr.ncasaux.camelplantuml.model.query;
+
+public class Parameters {
+
+    private final boolean connectRoutes;
+
+    public Parameters(boolean connectRoutes) {
+        this.connectRoutes = connectRoutes;
+    }
+
+    public boolean connectRoutes() {
+        return connectRoutes;
+    }
+}
