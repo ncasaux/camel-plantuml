557233175a8acb0374f5997c69a8c90291fdf141
ncasaux <nicolas.apo@gmail.com>
2020-12-26 17:10:24 +0100

    Testing jmxHost URI before starting

---
+++ camel-plantuml-jar/src/main/java/fr/ncasaux/camelplantuml/utils/JmxUtils.java
@@ -0,0 +1,14 @@
+package fr.ncasaux.camelplantuml.utils;
+
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import java.io.IOException;
+
+public class JmxUtils {
+    public static JMXConnector getConnector(String jmxHost) throws IOException {
+        String url = "service:jmx:rmi:///jndi/rmi://" + jmxHost + "/jmxrmi";
+        JMXServiceURL serviceURL = new JMXServiceURL(url);
+        return JMXConnectorFactory.connect(serviceURL);
+    }
+}
